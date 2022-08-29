45d92e0af0bfb4f89f1f0b2f5bc67a241ec0572b
Abhiraj <abhirajbhattashali@gmail.com>
2022-08-30 01:30:30 +0530

    Initial Commit

---
+++ Effect/EffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EffectApp.swift
+//  Effect
+//
+//  Created by Abhiraj on 30/08/22.
+//
+
+import SwiftUI
+
+@main
+struct EffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EffectUITests/EffectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  EffectUITestsLaunchTests.swift
+//  EffectUITests
+//
+//  Created by Abhiraj on 30/08/22.
+//
+
+import XCTest
+
+class EffectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
